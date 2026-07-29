6a752b86c56228989ecabb50ae7e23008b84d572
Tiago <tiagohammesmello@gmail.com>
2026-07-29 08:38:08 -0300

    alteração tiago

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1007,47 +1007,47 @@ const DecorativeShapes = () => (
 
     {/* Formas aleatórias espalhadas sem sobreposição */}
     <div className="fixed top-[25%] left-[12%] w-36 h-36 pointer-events-none z-0">
-      <div className="absolute top-0 left-0 w-full h-full bg-blue-300/6 rounded-br-full"></div>
+      <div className="absolute top-0 left-0 w-full h-full bg-green-500/8 rounded-br-full"></div>
     </div>
 
     <div className="fixed top-[18%] right-[18%] w-42 h-42 pointer-events-none z-0">
-      <div className="absolute top-0 right-0 w-full h-full bg-purple-300/8 rounded-bl-full"></div>
+      <div className="absolute top-0 right-0 w-full h-full bg-red-500/10 rounded-bl-full"></div>
     </div>
 
     <div className="fixed top-[42%] left-[35%] w-32 h-32 pointer-events-none z-0">
-      <div className="absolute top-0 left-0 w-full h-full bg-orange-300/7 rounded-tr-full"></div>
+      <div className="absolute top-0 left-0 w-full h-full bg-yellow-400/15 rounded-tr-full"></div>
     </div>
 
     <div className="fixed top-[55%] right-[32%] w-38 h-38 pointer-events-none z-0">
-      <div className="absolute top-0 right-0 w-full h-full bg-teal-300/9 rounded-tl-full"></div>
+      <div className="absolute top-0 right-0 w-full h-full bg-green-500/8 rounded-tl-full"></div>
     </div>
 
-    <div className="fixed bottom-[38%] left-[28%] w-40 h-40 pointer-events-none z-0">
-      <div className="absolute bottom-0 left-0 w-full h-full bg-indigo-300/8 rounded-br-full"></div>
+    <div className="fixed bottom-[38%] left-[18%] w-40 h-40 pointer-events-none z-0">
+      <div className="absolute bottom-0 left-0 w-full h-full bg-red-500/10 rounded-br-full"></div>
     </div>
 
     <div className="fixed bottom-[22%] right-[22%] w-34 h-34 pointer-events-none z-0">
-      <div className="absolute bottom-0 right-0 w-full h-full bg-pink-300/10 rounded-tl-full"></div>
+      <div className="absolute bottom-0 right-0 w-full h-full bg-red-500/10 rounded-tl-full"></div>
     </div>
 
     <div className="fixed top-[68%] left-[15%] w-36 h-36 pointer-events-none z-0">
-      <div className="absolute top-0 left-0 w-full h-full bg-cyan-300/6 rounded-tr-full"></div>
+      <div className="absolute top-0 left-0 w-full h-full bg-yellow-400/15 rounded-tr-full"></div>
     </div>
 
-    <div className="fixed top-[75%] left-[45%] w-32 h-32 pointer-events-none z-0">
-      <div className="absolute top-0 left-0 w-full h-full bg-lime-300/8 rounded-bl-full"></div>
+    <div className="fixed top-[75%] left-[25%] w-32 h-32 pointer-events-none z-0">
+      <div className="absolute top-0 left-0 w-full h-full bg-green-500/8 rounded-bl-full"></div>
     </div>
 
     <div className="fixed top-[32%] right-[8%] w-28 h-28 pointer-events-none z-0">
-      <div className="absolute top-0 right-0 w-full h-full bg-amber-300/7 rounded-br-full"></div>
+      <div className="absolute top-0 right-0 w-full h-full bg-yellow-400/15 rounded-br-full"></div>
     </div>
 
     <div className="fixed bottom-[45%] left-[58%] w-36 h-36 pointer-events-none z-0">
-      <div className="absolute bottom-0 left-0 w-full h-full bg-rose-300/9 rounded-tl-full"></div>
+      <div className="absolute bottom-0 left-0 w-full h-full bg-red-500/10 rounded-tl-full"></div>
     </div>
 
     <div className="fixed top-[85%] right-[15%] w-30 h-30 pointer-events-none z-0">
-      <div className="absolute top-0 right-0 w-full h-full bg-violet-300/6 rounded-tr-full"></div>
+      <div className="absolute top-0 right-0 w-full h-full bg-red-500/10 rounded-tr-full"></div>
     </div>
   </>
 );
